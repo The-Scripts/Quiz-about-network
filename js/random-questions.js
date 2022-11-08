0d12f2a5d7a76f11f34d881e4228cbdba7666b53
Łukasz Stępień <lukaszatepien@gmail.com>
2022-11-08 09:37:14 +0100

    Added 2 questrions

---
+++ js/random-questions.js
@@ -0,0 +1,9 @@
+const question1 = {
+    question: "Jakie urządzenia mają adres MAC?",
+    options: ["Sprzęt sieciowy", "Każde urządzenie", "Peryferia", "Procesory"]
+};
+
+const question2 = {
+    question: "Co to jest numer IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
