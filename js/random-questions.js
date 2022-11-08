602765b85f0a8b6ddde1ae1e73f7c27d20af0175
Łukasz Stępień <lukaszatepien@gmail.com>
2022-11-08 13:49:46 +0100

    Add some questions

--- js/random-questions.js
+++ js/random-questions.js
@@ -7,3 +7,38 @@ const question2 = {
     question: "Co to jest numer IP?",
     options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
 };
+
+const question3 = {
+    question: "Jakie mamy klasy numerów IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
+
+const question4 = {
+    question: " Jakie mamy klasy numerów IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
+
+const question5 = {
+    question: "Co to jest numer IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
+
+const question6 = {
+    question: "Co to jest numer IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
+
+const question7 = {
+    question: "Co to jest numer IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
+
+const question8 = {
+    question: "Co to jest numer IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
+
+const question9 = {
+    question: "Co to jest numer IP?",
+    options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
+};
