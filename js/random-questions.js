1f097dfb08b5be72527ff55b4449fc73725a4719
Łukasz Stępień <lukaszatepien@gmail.com>
2022-11-08 14:24:37 +0100

    Unworking code

--- js/random-questions.js
+++ js/random-questions.js
@@ -42,3 +42,29 @@ const question9 = {
     question: "Co to jest numer IP?",
     options: ["Ciąg liter i cyfr", "Lista urządzeń w sieci", "Maska podsieci", "Geolokalizacja urządzenia"]
 };
+
+const questions = [question1, question2, question3,
+    question4, question5, question6, question7, question8,
+    question9
+    ];
+
+let randomNumbers = [];
+
+for (let i = 0; i < 10; i++) {
+    let nextNumber = true;
+    let number;
+    do {
+        nextNumber = true
+        number = Math.round(Math.random() * 10);
+
+        for (let a = 0; a < i; a++) {
+            if (number == randomNumbers[a]) {
+                nextNumber = false;
+            }
+        }
+    } while (nextNumber == false);
+    
+    randomNumbers[i] += number; 
+
+    console.log(number)
+}
