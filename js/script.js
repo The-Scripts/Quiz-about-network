3e782fd4dcf2f01a2269682701dd011dc093658f
ArcySanta <arcysanta@gmail.com>
2022-11-12 10:38:36 +0100

    improved comments

--- js/script.js
+++ js/script.js
@@ -3,10 +3,7 @@ const prevBtn = document.getElementById("prev");
 const btnwrap = document.getElementById("buttons-wrapper");
 let questNumb = 0;
 
-//czas
-//pytania i odpowiedzi lista2D
-//tab co już było
-//licznik i wyświetlacz ilości pytań
+//czas do zrobienia
 
 function prevAndNext() {
   console.log(questNumb);
@@ -27,14 +24,11 @@ function checkPrevBtn() {
     btnwrap.style.display = "block";
   }
   nextBtn.style.width = "40vw";
-  // if(questNumb == 1)
-  //     prevBtn.setAttribute("disabled", "")
 }
 nextBtn.addEventListener("click", () => {
   //NEXT
   if (questNumb == 0) nextBtn.setAttribute("value", "Next");
-  if (questNumb < 5) {
-    //5 to ilość pytań (domyślnie ma być 10)
+  if (questNumb < 10) { //10 to ilość pytań
     document.getElementsByTagName("ul")[0].textContent = " "; //usuwa zasady
     questNumb++;
     prevAndNext();
