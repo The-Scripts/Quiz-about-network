3bc07c157a7f0c2a12278a5396ca253095fda28a
Łukasz Stępień <lukaszatepien@gmail.com>
2022-11-14 10:30:53 +0100

    New library

--- js/scriptPDF.js
+++ js/scriptPDF.js
@@ -1,5 +1,7 @@
-import { jsPDF } from "./jspdf";
+import { jsPDF } from "jspdf";
 
+// Default export is a4 paper, portrait, using millimeters for units
 const doc = new jsPDF();
 
-console.log("dsadsa");
+doc.text("Hello world!", 10, 10);
+doc.save("a4.pdf");
