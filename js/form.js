53dee3af86f3ad068ff4a16aba5d0135124d459f
Łukasz Stępień <lukaszatepien@gmail.com>
2022-12-01 22:19:14 +0100

    First load form and after fill, take user to quiz

---
+++ js/form.js
@@ -0,0 +1,12 @@
+let error = true;
+
+document.getElementById("submit-btn").addEventListener("click", (event) => {
+    if (error == false) {
+        window.location.href = "./quiz.html";
+    }
+    sessionStorage.setItem("user-name", document.getElementById("user-name").value);
+    sessionStorage.setItem("user-surname", document.getElementById("user-surname").value);
+    sessionStorage.setItem("user-email", document.getElementById("user-email").value);
+
+    
+})
