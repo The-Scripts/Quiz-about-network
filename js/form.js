60b4f4562a07eb89ed14d5d94a76edcb24be6b22
Łukasz Stępień <lukaszatepien@gmail.com>
2022-12-06 17:55:23 +0100

    Added  user name checking

--- js/form.js
+++ js/form.js
@@ -1,12 +1,40 @@
-let error = false;
+const userName = document.getElementById("user-name");
+const userSurname = document.getElementById("user-surname");
+const userEmail = document.getElementById("user-email");
+const errorElement = document.getElementById("error");
+let error = true;
 
 document.getElementById("submit-btn").addEventListener("click", (event) => {
     if (error == false) {
         window.location.href = "./quiz.html";
+        sessionStorage.setItem("user-name", userName.value.trim());
+        sessionStorage.setItem("user-surname", userSurname.value.trim());
+        sessionStorage.setItem("user-email", userEmail.value.trim());
     }
-    sessionStorage.setItem("user-name", document.getElementById("user-name").value);
-    sessionStorage.setItem("user-surname", document.getElementById("user-surname").value);
-    sessionStorage.setItem("user-email", document.getElementById("user-email").value);
+})
 
+document.getElementById("user-name").addEventListener("focusout", () => {
+    let userNameText = userName.value.trim();
+    for (let i = 0; i < userNameText.length; i++) {
+        if ((userNameText.char > 64 && userNameText.charCodeAt(i) < 90) || 
+        (userNameText.charCodeAt(i) > 96 && userNameText.charCodeAt(i) < 123)) {
+            error = false;
+            if (i == userNameText.length - 1) {
+                document.getElementById("error-name").remove();
+            }
+            console.log(userNameText.charCodeAt(i));
+        } else {
+            error = true;
+            break;
+        }
+    }
     
+
+    if (error && errorElement.children.length == 0) {
+        const errorInfo = document.createElement('p');
+        errorInfo.id = "error-name";
+        errorInfo.textContent = "Wpisz poprawne imię";
+        errorElement.appendChild(errorInfo);
+        errorElement.style.display = "block";
+    }
 })
