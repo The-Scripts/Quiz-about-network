6f6a80b335f43b9e6c9285a9d36230168925d8ff
Łukasz Stępień <lukaszatepien@gmail.com>
2022-12-08 19:42:39 +0100

    Addded checking email

--- js/form.js
+++ js/form.js
@@ -13,10 +13,11 @@ const showingInfoError = (infoContent, errorName) => {
 }
 
 const checkNames = (inputToCheck) => {
-    let userNameText = document.getElementById(inputToCheck).value.trim();
+    const userNameText = document.getElementById(inputToCheck).value.trim();
     for (let i = 0; i < userNameText.length; i++) {
-        if ((userNameText.char > 64 && userNameText.charCodeAt(i) < 90) || 
-        (userNameText.charCodeAt(i) > 96 && userNameText.charCodeAt(i) < 123)) {
+        const charInI = userNameText.charCodeAt(i);
+        if ((charInI > 64 && charInI < 90) || 
+        (charInI > 96 && charInI < 123)) {
             error = false;
         } else {
             error = true;
@@ -25,16 +26,16 @@ const checkNames = (inputToCheck) => {
     }
 
     if (error && inputToCheck == "user-name") {
-        showingInfoError("Wpisz poprawne imie", 1, inputToCheck);
+        showingInfoError("Wpisz poprawne imie", inputToCheck);
     } else if (error) {
-        showingInfoError("Wpisz poprawne nazwisko", 1, inputToCheck);
+        showingInfoError("Wpisz poprawne nazwisko", inputToCheck);
     } else if (error == false) {
         document.getElementById(`error-${inputToCheck}`).remove();
     }
 }
 
 document.getElementById("submit-btn").addEventListener("click", (event) => {
-    if (error == false) {
+    if (error == false && userName.value != "" && userSurname.value != "" && userEmail.value != "") {
         window.location.href = "./quiz.html";
         sessionStorage.setItem("user-name", userName.value.trim());
         sessionStorage.setItem("user-surname", userSurname.value.trim());
@@ -45,5 +46,29 @@ document.getElementById("submit-btn").addEventListener("click", (event) => {
 document.getElementById("user-form").addEventListener("focusout", (event) => {
     if (event.target.id == "user-name" || event.target.id == "user-surname") {
         checkNames(event.target.id);
+    } else if (event.target.id == "user-email") {
+        const userEmailText = userEmail.value.trim();
+        for (let i = 0; i < userEmailText.length; i++) {
+            const charInI = userEmailText.charCodeAt(i);
+            if ((charInI > 63 && charInI < 90) || 
+            (charInI > 96 && charInI < 123)  ||
+            (charInI > 47 && charInI < 58) ||
+            (charInI == 46)) {
+                error = false;
+            } else {
+                error = true;
+                break;
+            }
+        }
+
+        if (error) {
+            showingInfoError("Wpisz poprawny email", "user-email");
+        } else if (error == false) {
+            document.getElementById(`error-user-email`).remove();
+        }
+    } 
+
+    if (errorElement.children.length == 0) {
+        errorElement.remove();
     }
 })
