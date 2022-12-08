316d18dadc5ad50afc7eae997596df528d48603d
Łukasz Stępień <lukaszatepien@gmail.com>
2022-12-08 18:58:36 +0100

    Added checking surname and refactor: checking name

--- js/form.js
+++ js/form.js
@@ -4,6 +4,35 @@ const userEmail = document.getElementById("user-email");
 const errorElement = document.getElementById("error");
 let error = true;
 
+const showingInfoError = (infoContent, errorName) => {
+    const errorInfo = document.createElement('p');
+    errorInfo.id = `error-${errorName}`;
+    errorInfo.textContent = infoContent;
+    errorElement.appendChild(errorInfo);
+    errorElement.style.display = "block";
+}
+
+const checkNames = (inputToCheck) => {
+    let userNameText = document.getElementById(inputToCheck).value.trim();
+    for (let i = 0; i < userNameText.length; i++) {
+        if ((userNameText.char > 64 && userNameText.charCodeAt(i) < 90) || 
+        (userNameText.charCodeAt(i) > 96 && userNameText.charCodeAt(i) < 123)) {
+            error = false;
+        } else {
+            error = true;
+            break;
+        }
+    }
+
+    if (error && inputToCheck == "user-name") {
+        showingInfoError("Wpisz poprawne imie", 1, inputToCheck);
+    } else if (error) {
+        showingInfoError("Wpisz poprawne nazwisko", 1, inputToCheck);
+    } else if (error == false) {
+        document.getElementById(`error-${inputToCheck}`).remove();
+    }
+}
+
 document.getElementById("submit-btn").addEventListener("click", (event) => {
     if (error == false) {
         window.location.href = "./quiz.html";
@@ -13,28 +42,8 @@ document.getElementById("submit-btn").addEventListener("click", (event) => {
     }
 })
 
-document.getElementById("user-name").addEventListener("focusout", () => {
-    let userNameText = userName.value.trim();
-    for (let i = 0; i < userNameText.length; i++) {
-        if ((userNameText.char > 64 && userNameText.charCodeAt(i) < 90) || 
-        (userNameText.charCodeAt(i) > 96 && userNameText.charCodeAt(i) < 123)) {
-            error = false;
-            if (i == userNameText.length - 1) {
-                document.getElementById("error-name").remove();
-            }
-            console.log(userNameText.charCodeAt(i));
-        } else {
-            error = true;
-            break;
-        }
-    }
-    
-
-    if (error && errorElement.children.length == 0) {
-        const errorInfo = document.createElement('p');
-        errorInfo.id = "error-name";
-        errorInfo.textContent = "Wpisz poprawne imię";
-        errorElement.appendChild(errorInfo);
-        errorElement.style.display = "block";
+document.getElementById("user-form").addEventListener("focusout", (event) => {
+    if (event.target.id == "user-name" || event.target.id == "user-surname") {
+        checkNames(event.target.id);
     }
 })
